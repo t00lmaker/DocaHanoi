c26ed6b4bee6ac1b4034e3a09822064dc9cd2cc6
luan <luan@infoway-pi.com.br>
2019-11-05 00:36:32 -0300

    adicionar validações ao modelo

--- src/com/luanpontes/hanoi/model/Delivery.java
+++ src/com/luanpontes/hanoi/model/Delivery.java
@@ -1,6 +1,48 @@
 package com.luanpontes.hanoi.model;
 
-public class Delivery {
-	
+import java.io.Serializable;
+import java.util.Collection;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
+public class Delivery implements Serializable{
+	
+	private static final long serialVersionUID = -7718146278982486408L;
+	
+	@NotEmpty
+	@NotNull
+	private String vehicle;
+	
+	@NotEmpty
+	@NotNull
+	private String deliveryId;
+	
+	@NotEmpty
+	private Collection<Package> packages;
+
+	public String getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(String vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public String getDeliveryId() {
+		return deliveryId;
+	}
+
+	public void setDeliveryId(String deliveryId) {
+		this.deliveryId = deliveryId;
+	}
+
+	public Collection<Package> getPackages() {
+		return packages;
+	}
+
+	public void setPackages(Collection<Package> packages) {
+		this.packages = packages;
+	}
 }
--- src/com/luanpontes/hanoi/model/Package.java
+++ src/com/luanpontes/hanoi/model/Package.java
@@ -1,15 +1,27 @@
 package com.luanpontes.hanoi.model;
 
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 /**
  * Representação de um pacote.
  * 
  * @author toolmaker
  *
  */
-public class Package {
+public class Package implements Serializable {
 	
+	private static final long serialVersionUID = -8659352172809275400L;
+	
+	@NotEmpty 
+	@NotNull
 	private String id;
 	
+	@Positive
+	@NotNull
 	private Double weight;
 	
 	public Package(String id, Double weight) {
